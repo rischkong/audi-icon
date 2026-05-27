99c11516af95cb5f00b7463e1ff353151845a25d
Thomas Michelbach <t.michelbach@sp.design>
2026-05-27 09:01:32 +0200

    feat: updated icons for 4.1

--- dist/react/FrontDoorLeftXs.js
+++ dist/react/FrontDoorLeftXs.js
@@ -7,12 +7,10 @@ const SvgFrontDoorLeftXs = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path fill="currentColor" d="M4 16H3v-3.5h1zm7 0H5v-1h6z" />
     <path
       fill="currentColor"
-      d="M8.83 0q.57 0 1.13.16l2.03.58c.6.17 1.01.72 1.01 1.34V16h-1V2.08a.4.4 0 0 0-.29-.38l-2.03-.58A3 3 0 0 0 8.83 1H6.8q-.51 0-.99.16l-1.53.51a.4.4 0 0 0-.27.38v5.62L.9 11.8l-.8-.6L3 7.33V2.05c0-.6.38-1.13.95-1.32L5.48.2a4 4 0 0 1 1.3-.2z"
+      d="M4 12v4H3v-4zm7 3v1H5v-1zM3 2.05c0-.6.38-1.13.95-1.32L5.48.2a4 4 0 0 1 1.3-.2h2.05q.57 0 1.13.16l2.03.58c.6.17 1.01.72 1.01 1.34V16h-1V2.08a.4.4 0 0 0-.29-.38l-2.03-.58A3 3 0 0 0 8.83 1H6.8q-.51 0-.99.16l-1.53.51a.4.4 0 0 0-.27.38v6.16L.85 11.35l-.7-.7L3 7.79zM11 7v1H5V7z"
     />
-    <path fill="currentColor" d="M11 8H5V7h6z" />
   </svg>
 );
 export default SvgFrontDoorLeftXs;
---
+++ dist/react/GpsInfoS.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+const SvgGpsInfoS = (props) => (
+  <svg
+    width={24}
+    height={24}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M23 19.5a3.5 3.5 0 1 0-7 0 3.5 3.5 0 0 0 7 0M12.5.5v2.01a9.46 9.46 0 0 1 8.99 8.99h2.01v1h-2.01q-.04.83-.22 1.6l-.98-.2q.15-.69.2-1.4H17.5v-1h2.98a8.45 8.45 0 0 0-7.98-7.99V6.5h-1V3.51a8.45 8.45 0 0 0-7.98 7.99H6.5v1H3.52a8.45 8.45 0 0 0 7.98 7.98V17.5h1v2.98q.71-.03 1.4-.19l.2.98q-.77.18-1.6.22v2.01h-1v-2.01a9.46 9.46 0 0 1-8.99-8.99H.5v-1h2.01a9.46 9.46 0 0 1 8.99-8.99V.5zM20 19v3h-1v-3zm0-2v1h-1v-1zm4 2.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0"
+    />
+  </svg>
+);
+export default SvgGpsInfoS;
